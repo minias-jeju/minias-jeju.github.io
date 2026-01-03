d548eac46fb121d73bc2b3fccd295526be2e12c9
minias <gnuhacker@gmail.com>
2026-01-04 00:44:10 +0900

    0.0.1 build to docs changed

---
+++ docs/_app/immutable/entry/start.BkZr8i3F.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/DXFb0Pas.js";export{o as load_css,r as start};
--- svelte.config.js
+++ svelte.config.js
@@ -12,6 +12,8 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
+ 			pages: 'docs',     // ⬅ GitHub Pages 웹루트
+	      	assets: 'docs',      		
 			fallback: 'index.html'   // ✅ SPA 핵심
 		})
 	}
