14ac783200b6dc31c3ad1cb97ba68c108d6080bf
minias <gnuhacker@gmail.com>
2026-01-04 02:05:45 +0900

    docs build

--- start.DgMQvvJd.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"Cjon4wD5.js";export{o as load_css,r as start};
--- svelte.config.js
+++ svelte.config.js
@@ -12,8 +12,8 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			pages: 'public',     // ⬅ GitHub Pages 웹루트
-			assets: 'public',
+			pages: 'docs',     // ⬅ GitHub Pages 웹루트
+			assets: 'docs',
 			fallback: 'index.html'   // ✅ SPA 핵심
 		}),
 		paths: {
