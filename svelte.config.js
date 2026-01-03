401cbf18a41fa25e9e0a8ed0ceb27f4192b743d6
minias <gnuhacker@gmail.com>
2026-01-04 01:08:33 +0900

    empty bug fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const repo = 'minias-jeju.github.io';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
@@ -19,7 +17,7 @@ const config = {
 			fallback: 'index.html'   // ✅ SPA 핵심
 		}),
 		paths: {
-			base: `/${repo}`
+			base: ''
 		},
 	}
 };
