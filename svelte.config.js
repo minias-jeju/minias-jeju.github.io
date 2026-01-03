f32077d960d1095f29cb2b672a3639c0ab9e3ce2
minias <gnuhacker@gmail.com>
2026-01-04 00:58:15 +0900

    build config update

--- svelte.config.js
+++ svelte.config.js
@@ -12,10 +12,14 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
- 			pages: 'docs',     // ⬅ GitHub Pages 웹루트
-	      	assets: 'docs',      		
+			pages: 'docs',     // ⬅ GitHub Pages 웹루트
+			assets: 'docs',
 			fallback: 'index.html'   // ✅ SPA 핵심
-		})
+		}),
+		paths: {
+			base: '/minias-jeju.github.io',
+			assets: '/minias-jeju.github.io'
+		},
 	}
 };
 
