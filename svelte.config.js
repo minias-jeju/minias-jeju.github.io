c4f3eaf691929d0216d61db4bfdecc6e4a23c0dd
minias <gnuhacker@gmail.com>
2026-01-05 09:26:50 +0900

    0.0.2 리빌드

--- svelte.config.js
+++ svelte.config.js
@@ -4,6 +4,7 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
 	kit: {
 		adapter: adapter({
+			pages: 'docs',   // 빌드 결과 html/js/css가 들어갈 폴더
 			fallback: '404.html'
 		}),
 		paths: {      		
