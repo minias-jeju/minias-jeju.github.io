d1f9065f5cf99409d8acc38bc381441e2a580d7c
minias <gnuhacker@gmail.com>
2026-01-05 09:41:01 +0900

    CI/CD update

--- svelte.config.js
+++ svelte.config.js
@@ -2,15 +2,17 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			pages: 'docs',   // 빌드 결과 html/js/css가 들어갈 폴더
-			fallback: '404.html'
-		}),
-		paths: {      		
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
-	}
+  kit: {
+    adapter: adapter({
+      pages: 'docs',     // 빌드 결과 폴더
+      assets: 'docs',
+      fallback: 'index.html',  // SPA 라우팅
+      precompress: false
+    }),
+    paths: {
+      base: ''   // 커스텀 도메인은 base path 없음
+    }
+  }
 };
 
 export default config;
